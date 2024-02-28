528d1341bc637893bad71accb95f1297323002d5
Tom Saury <saurytom@gmail.com>
2024-02-28 13:56:26 +0100

    init UI hierarchy

---
+++ Assets/_Script/UI/MainMenuBehaviour.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuBehaviour : MonoBehaviour
+{
+	// Start is called before the first frame update
+	private void Start()
+	{
+
+	}
+
+	public void StartPlayerVsMachine()
+	{
+
+	}
+
+	public void StartPlayerVsPlayer()
+	{
+
+	}
+
+	public void Quit()
+	{
+		Application.Quit();
+	}
+}
